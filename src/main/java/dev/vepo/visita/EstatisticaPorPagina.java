88b0a2fd4f76b0009a21ea5b84185b07c41557b9
Victor Osório <victor.perticarrari@gmail.com>
2026-01-08 14:03:17 -0300

    HOTFIX!!!

--- src/main/java/dev/vepo/visita/EstatisticaPorPagina.java
+++ src/main/java/dev/vepo/visita/EstatisticaPorPagina.java
@@ -1,5 +1,8 @@
 package dev.vepo.visita;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
+@RegisterForReflection
 public record EstatisticaPorPagina(String pagina, Long visitas, Double tempoMedio, Long avg70, Long avg90) {
     public EstatisticaPorPagina(Object pagina, Object visitas, Object tempoMedio, Object avg70, Object avg90) {
         this((String) pagina, (Long) visitas, (Double) tempoMedio, (Long) avg70, (Long) avg90);
