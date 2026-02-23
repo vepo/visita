a2903ccf0e445bda5afc84a88c1177e5a4384050
Victor Osório <victor.perticarrari@gmail.com>
2026-02-23 17:21:05 -0300

    #61 Implement view

--- src/main/java/dev/vepo/visita/dashboard/StatsRepository.java
+++ src/main/java/dev/vepo/visita/dashboard/StatsRepository.java
@@ -183,7 +183,7 @@ public class StatsRepository {
     public List<RefererStats> findAllRefererStats(Selector selector, String parameter) {
         return switch (selector) {
             case DOMAIN -> entityManager.createQuery("""
-                                                     SELECT new RefererStats(v.referer,
+                                                     SELECT new RefererStats(v.originalView.referer,
                                                          COUNT(v.id) as views,
                                                          AVG(v.length) as avgDuration,
                                                          PERCENTILE_DISC(0.7) WITHIN GROUP (ORDER BY v.length) as avgDurationPerc50,
@@ -200,7 +200,7 @@ public class StatsRepository {
                                         .getResultStream()
                                         .toList();
             case REFERRER -> entityManager.createQuery("""
-                                                       SELECT new RefererStats(v.referer,
+                                                       SELECT new RefererStats(v.originalView.referer,
                                                            COUNT(v.id) as views,
                                                            AVG(v.length) as avgDuration,
                                                            PERCENTILE_DISC(0.7) WITHIN GROUP (ORDER BY v.length) as avgDurationPerc50,
